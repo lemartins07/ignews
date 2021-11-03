3c57a682f81d69d22e78869158364b9627fee8e4
Leandro Martins <leandro.martins.85@gmail.com>
2021-11-03 18:03:25 -0300

    Componente: SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
 
@@ -19,6 +20,8 @@ export default function Home() {
             Get access to alt the publications <br />
             <span>for $9.90</span>
           </p>
+
+          <SubscribeButton />
         </section>
         
         <img src="/images/avatar.svg" alt="Girl coding" />
