bd9b1cd0835aeee04ad8de4d3c4c5e0290e683e3
Leandro Martins <leandro.martins.85@gmail.com>
2021-11-03 17:37:58 -0300

    Página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,28 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Ig.News</title>
-      </Head>
-      <h1>Hello World</h1>
+        <title>Home | Ig.News</title>
+      </Head>      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the 
+            <span> React</span> world
+          </h1>
+          <p>
+            Get access to alt the publications <br />
+            <span>for $9.90</span>
+          </p>
+        </section>
+        
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
