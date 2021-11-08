3d5d8aac8c34853076027ed8cd3934cd333afb8d
Leandro Martins <leandro.martins.85@gmail.com>
2021-11-07 21:15:49 -0300

    Salvando usuário no FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -10,5 +14,23 @@ export default NextAuth({
       scope: 'read:user'
     }),
     // ...add more providers here
-  ],  
+  ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+      
+      try { 
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email }}
+          )
+        )
+        return true;
+      } catch (e){
+        console.log(e);
+        return false;
+      }   
+    }
+  }  
 })
