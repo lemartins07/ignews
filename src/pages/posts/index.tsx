2ccb9a32b0e1b943151cc1ecd29d503c04fb123f
Leandro Martins <leandro.martins.85@gmail.com>
2021-11-10 19:24:29 -0300

    Listando posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,37 +1,39 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
-import { getPrismicClient } from '../../services/prismic';
-import styles from './styles.module.scss';
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+import { getPrismicClient } from '../../services/prismic';
 
+import styles from './styles.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostProps {
+  posts: Post[]
+}
+
+export default function Posts({posts}:PostProps) {
   return (
     <>
       <Head>
         <title>Posts | ig.news</title>
       </Head>
-
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>08 de novembro de 2021</time>
-            <strong>adahdjkahdja</strong>
-           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto tenetur omnis deleniti quidem amet culpa in facere consequatur, ducimus iste impedit voluptate harum! Recusandae odio nobis, sed pariatur facilis beatae.</p>
-          </a>
-          <a href="#">
-            <time>08 de novembro de 2021</time>
-            <strong>adahdjkahdja</strong>
-           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto tenetur omnis deleniti quidem amet culpa in facere consequatur, ducimus iste impedit voluptate harum! Recusandae odio nobis, sed pariatur facilis beatae.</p>
-          </a>
-          <a href="#">
-            <time>08 de novembro de 2021</time>
-            <strong>adahdjkahdja</strong>
-           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto tenetur omnis deleniti quidem amet culpa in facere consequatur, ducimus iste impedit voluptate harum! Recusandae odio nobis, sed pariatur facilis beatae.</p>
-          </a>
+          {posts.map(post => (
+            <a key ={post.slug} href="#" >
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
-
     </>
   );
 }
@@ -46,9 +48,22 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100,
   });
 
-  console.log(response);
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })  
 
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }
